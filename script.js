dbc23200ee50af07245d97a85bd8a36cd57d12bf
Z3N0-2026 <discordmain573@gmail.com>
2026-02-11 18:49:57 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Typing effect text
+const text = "Z3N0 • Discord Manager • JavaScript Coder • Bot development • Moderation";
+const typingElement = document.querySelector(".typing-text");
+let index = 0;
 
+function type() {
+  if (index < text.length) {
+    typingElement.textContent += text[index];
+    index++;
+    setTimeout(type, 100); // typing speed (ms)
+  }
+}
+
+// Start typing after page load
+window.addEventListener("load", type);
